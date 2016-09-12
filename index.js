e9eb24fe2ea717084adcd7d276b84322ffd1c4be
DSikes <pockybum522@gmail.com>
2016-09-11 22:02:54 -0400

    Made SSH setup not global so it stopped registering multiple times on subsequent runs
    Fixes bug where commands would get run a bunch of times upon multiple
    runs

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 var SSH = require('simple-ssh');
 var Alexa = require("alexa-sdk");
 
-var ssh = new SSH({
-	host: 'yourserver.com',
-	user: 'username',
-  	key: `-----BEGIN RSA PRIVATE KEY-----
+exports.handler = function(event, context, callback) {
+    var ssh = new SSH({
+        host: 'yourserver.com',
+        user: 'username',
+        key: `-----BEGIN RSA PRIVATE KEY-----
 MIIEpAIBAAKCAQEApQtRxugrDMU8YNBy2j2Lyk6yAxMSKaiusrNUamXKLxFvdlZ1
-6HCN+jjaE7q8OYFEmq9l2B5U8GCYMFldXWBxIv7fvRWyi1ZTw3olaZ8DmGYwPKLM
+qllnnUwLPC1P/2jPXRtmavLhhiAUrPVpC12WjZMlYTxQc9WbjXHXx24MgPa8jeAw
 TOQ3MOm/zcJZbiTY1/cx2CW6NupPX78O42hLKM2iJwp6epgxC5t2Vw==
 -----END RSA PRIVATE KEY-----`
-});
+    });
 
-exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
 	alexa.registerHandlers(handlers);
 
 	ssh.on("close", function () {alexa.execute()});
 	ssh
-        .exec('nohup /home/username/script.sh > /dev/null 2>&1 &', {  // Nohup runs script in BG, > /dev/null redirects output. & also runs in BG. Trying to keep things fast to make alexa response time good.
+        .exec('nohup /path/to/script.sh </dev/null &>/dev/null &', {  // Nohup runs script in BG, < /dev/null &> redirects input and output. & also runs in BG. Trying to keep things fast to make alexa response time good.
             out: console.log.bind(console)
         })
-        .exec('exit', {                                               // Also for trying to keep things fast.
+        .exec('exit', {                                            // Also for trying to keep things fast.
             out: console.log.bind(console)
         }).start();	
 };
 
 var handlers = {
     'LaunchRequest': function(){
-		this.emit(':tell', 'Now opening the garage door.'); 
+		this.emit(':tell', 'Script run.'); 
     }
 };
